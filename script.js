e8f1ed7372f194eff709461fc853478064fd52f7
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2026-01-12 11:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let form = document.getElementById('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    let name = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let phone = document.getElementById('phonenumber').value;
+    let email = document.getElementById('emailid').value;
+    alert(`First Name: ${name}\nLast Name: ${lastname}\nPhone Number: ${phone}\nEmail ID: ${email}`);
+});
